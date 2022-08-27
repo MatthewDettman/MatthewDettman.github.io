6463e5ad7a15bfa09d1a64d6cbb218e2148524eb
MD2020 <matthew.dettman@gmail.com>
2022-08-26 17:30:22 -0700

    some changes

---
+++ scripts/setup.R
@@ -0,0 +1,33 @@
+pkgs <- c(
+  "rmarkdown",
+  "tidyverse", 
+  "tidyquant",  
+  "plotly",
+  "blogdown",
+  "showtext",
+  "fs",
+  "shiny",
+  "shinyWidgets",
+  "shinythemes",
+  "shinyjs",
+  "config",
+  "scales", 
+  "flexdashboard",
+  "xgboost",
+  "parsnip",
+  "timetk",
+  "modeltime",
+  "lubridate",
+  "DT",
+  "devtools",
+  "blogdown",
+  "portfoliodown",
+  "bslib",
+  "Rtools",
+  "devtools"
+)
+
+install.packages(pkgs)
+
+
+#devtools::install_github("business-science/portfoliodown")
